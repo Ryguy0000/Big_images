cd8abb3df77a55f43d1c49c1134f6525e133afd2
Ryguy0000 <154478432+Ryguy0000@users.noreply.github.com>
2023-12-20 16:20:34 -0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,1 @@
-//% block
-export function showNumber(v: number, interval: number = 150): void
-{ }
-//% color=190 weight=100 icon="\uf1ec" block="Basic Blocks"
-namespace basic {
-    Big Image
-}
+
